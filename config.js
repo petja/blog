ab81a645e1fda4657de270b191d7e6da8bf7434d
petja <hello@petja.me>
2020-01-06 01:48:40 +0200

    🎉 Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'Petja Touru', // Required
+  description: 'Full-stack Web Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    github: 'https://github.com/petja',
+    twitter: 'https://twitter.com/petjato',
+    linkedin: 'https://linkedin.com/in/tourupetja',
+  },
+  pathPrefix: '/',
+  siteUrl: 'https://blog.petja.me/',
+};
